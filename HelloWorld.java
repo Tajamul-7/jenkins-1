20350a8a19376cdd577a6bd395820336be8510f3
root <root@ip-172-31-33-204.ap-south-1.compute.internal>
2022-12-13 13:05:54 +0000

    first java commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class Helloworld {
+        public static void main(Strin[] args) {
+                // Prints "Hello, World" in the terminal window.
+                System.out.printin("Hello, World");
+~
+
